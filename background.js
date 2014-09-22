f5132c10c08116d1f75eeb26532bb3b6df7f0b8a
duheeye <pitercat07@gmail.com>
2014-09-22 20:33:43 +0900

    Revert "store complete"
    This reverts commit ebdb9454cb1f6dd6d01d849b7a12e9701f3a7d6a.

--- background.js
+++ background.js
@@ -26,39 +26,34 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
           //for(var i = 0; i < obj[key].length; i++) {
            // console.log(obj[key][i].name,":",obj[key][i].value);
           var allkeys = Object.keys(obj);
+          console.log(allkeys);
           
           if(allkeys.lastIndexOf(url) >= 0){
             key = url;
             data = {lastPass: request.time, count: obj[key].count+1};
             arr[key] = data;
             chrome.storage.sync.set(arr, function() {
-              console.log("exist!");
+              console.log("exist! and Store!");
             })
+            chrome.storage.sync.get(null, function(obj){
+              allkeys = Object.keys(obj);
+              console.log(obj[key]);
+            });
+            
           }else
           {
             key = url;
             data = {lastPass: request.time, count: 1}
             arr[key] = data;
             chrome.storage.sync.set(arr, function() {
-              console.log("not exist!");
+              console.log("not exist! and Store!");
+            });
+            chrome.storage.sync.get(null, function(obj) {
+              allkeys = Object.keys(obj);
+              console.log(obj[key]);
             });
           }
           //}
-          arr={};
-          key = "time#"+request.time;
-          data = request.data;
-          arr[key] = data;
-          chrome.storage.sync.set(arr, function() {
-              console.log("Store!");
-            });
-            
-          //chrome.storage.sync.clear();
-          
-          chrome.storage.sync.get(null, function(obj) {
-            allkeys = Object.keys(obj);
-            console.log(allkeys);
-            console.log(obj["url#"+request.url]);
-          });
       });
      
       
