a0cd157803dfdb242e7629c44a6a56caac2e0cc2
BoSung Kim <bokor.kim@gmail.com>
2014-10-23 18:19:35 +0900

    edit bug of suspicious
    done

--- SuspiciousTable.js
+++ SuspiciousTable.js
@@ -6,7 +6,7 @@
  * it will process the table afterall.
  * Author: InHwan Kim
  */
-function getValue(callback) { 
+function getValue(callback) {
 	chrome.storage.sync.get(null, callback);
 }
 
@@ -27,7 +27,7 @@ function getFilteredData(allObj, type) {
 		// parsedStr[0] -> time#month
 		// parsedStr[1] -> day
 		// parsedStr[2] -> year + ' ' + data
-		
+
 		var posItem = allObj[itemKey].length-2;
 		//alert(allObj[itemKey][posItem].hostName);
 		var tokenedData = itemKey.split('#');
@@ -42,8 +42,8 @@ function getFilteredData(allObj, type) {
 
 /*
  * Function name: initTbl
- * Description: 
- * Callback for chrome.storage.sync.get() function. 
+ * Description:
+ * Callback for chrome.storage.sync.get() function.
  * This will be passed to the sync.get() function.
  * This callback will get all the data from sync server,
  * and then put it to the table.
@@ -51,29 +51,29 @@ function getFilteredData(allObj, type) {
  * Author: BoSung Kim
  */
 function initTbl(obj) {
-  
+
   var keyList = getFilteredData(obj, "url");
 
   //var d = new Date();
   //var day = (d.getMonth()+1)+"/"+d.getDate()+"/"+d.getFullYear();
-  
 
-  
+
+
   // set current date to HTML
   var mDate = new Date();
   mLocalTime = (mDate.getMonth()+1) + '/' + mDate.getDate() + '/' + mDate.getFullYear();
   var mTime = document.getElementById("local_time");
   mTime.innerHTML = "Today: " + mLocalTime;
-  
-  
+
+
   //var day = "9/25/2014";
 
   var timeList = getFilteredData(obj, "time");
-  
+
   var table = document.getElementById("suspicious_list");
-  
+
   for(var i = 0; i < keyList.length; i++) {
- 
+
 /*
  * Suspicious or warning site are displayed based on values set by the user.
  * default suspicious value is 10 and warning value is 20.
@@ -82,7 +82,7 @@ function initTbl(obj) {
     var url = keyList[i].split('#');
     var objLen = obj[keyList[i]].length;
     var count = 0;
-    
+
     for(var k =0; k<timeList.length ; k++){
         var timeToken = timeList[k].split('#');
         var storedDay = timeToken[1].split(" ");
@@ -90,35 +90,33 @@ function initTbl(obj) {
           && mLocalTime == storedDay[0])
           count++;
     }
-    
+
     var setting = getFilteredData(obj, "setting");
     var suspicious;
     var warning;
-    
+
     if(setting.length == 0){
       suspicious = 10;
       warning = 20;
-    }else{
-      suspicious = obj[setting[0]][0].value;
-      warning = obj[setting[0]][1].value;
+    } else{
+        suspicious = obj[setting[0]][0].value;
+        warning = obj[setting[0]][1].value;
     }
-    
+
     if(count > suspicious){
 		  var row = table.insertRow(1);
 		  var cell_site = row.insertCell(0);
 		  var cell_access_num = row.insertCell(1);
 		  var cell_status = row.insertCell(2);
-		  var cell_lateset = row.insertCell(3);
 
 		  cell_site.innerHTML = url[1];
-		cell_access_num.innerHTML = count;
+		  cell_access_num.innerHTML = count;
 
 		  if(count > warning)
 			  cell_status.innerHTML = "Warnig";
 		  else if(count > suspicious)
 			  cell_status.innerHTML = "Suspicious";
-
-	}
+	  }
   }
 }
 
